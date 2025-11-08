f263380fe7f4270fc4b2d81d96d615ca9ab86243
akashpk41 <akashpk84100@gmail.com>
2025-11-08 10:22:03 +0600

    router setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import Call from "./pages/Call";
+import Chat from "./pages/Chat";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Notifications from "./pages/Notifications";
+import Onboarding from "./pages/Onboarding";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div className="h-screen" >
-     <Routes>
-      
-     </Routes>
+    <div className="h-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/call" element={<Call />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
