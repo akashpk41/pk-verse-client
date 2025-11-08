7558823d0dc005e0e3f88955593b397f0b3b9527
akashpk41 <akashpk84100@gmail.com>
2025-11-08 11:11:20 +0600

    created a custom hook for auth user

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
 import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
+import useAuth from "./hooks/useAuth";
 import Call from "./pages/Call";
 import Chat from "./pages/Chat";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Notifications from "./pages/Notifications";
 import Onboarding from "./pages/Onboarding";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
+  const authUser = useAuth();
+  console.log(authUser);
+
   return (
     <div className="h-screen">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/call" element={<Call />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <Notifications /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/onboarding"
+          element={authUser ? <Onboarding /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={authUser ? <Chat /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/call"
+          element={authUser ? <Call /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
       </Routes>
       <Toaster />
     </div>
